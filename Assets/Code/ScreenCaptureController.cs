760658991b808a1ebcdd563aad02e0e63c6238f5
One Minute <oneminutegamesbusiness@gmail.com>
2021-11-07 18:18:11 +0100

    Create scene for box list capture.

---
+++ Assets/Code/ScreenCaptureController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScreenCaptureController : MonoBehaviour {
+
+    public int ResolutionFactor = 5;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ".png", ResolutionFactor);
+            UnityEditor.AssetDatabase.Refresh();
+        }
+    }
+}
